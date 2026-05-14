28c315fe04199618e99f644757f15966e1ad280a
nathanBarloy <nbarloy@gmail.com>
2026-05-14 10:51:32 +0200

    feat: Add player movements

--- no-crunch-2026/player/player.gd
+++ no-crunch-2026/player/player.gd
@@ -1,1 +1,25 @@
 extends Node2D
+
+@export var speed = 400  # (pixel/s)
+
+
+func _process(delta: float) -> void:
+	var frame_speed = get_frame_speed(delta)
+	self.position += frame_speed
+
+
+func get_frame_speed(delta: float) -> Vector2:
+	var frame_speed = Vector2.ZERO
+	if Input.is_action_pressed("move-right"):
+		frame_speed.x += 1
+	if Input.is_action_pressed("move-left"):
+		frame_speed.x -= 1
+	if Input.is_action_pressed("move-up"):
+		frame_speed.y -= 1
+	if Input.is_action_pressed("move-down"):
+		frame_speed.y += 1
+	
+	if frame_speed.length() != 0:
+		frame_speed = frame_speed.normalized() * speed * delta
+	
+	return frame_speed
