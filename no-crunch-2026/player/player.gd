09876540a0d174a4a81e30e0ab2a14b7d8867d3f
nathanBarloy <nbarloy@gmail.com>
2026-05-14 11:36:37 +0200

    feat: Add action commands and signals

--- no-crunch-2026/player/player.gd
+++ no-crunch-2026/player/player.gd
@@ -2,10 +2,18 @@ extends Node2D
 
 @export var speed = 400  # (pixel/s)
 
+signal drop_request(node: Node)
+signal take_request()
+
 
 func _process(delta: float) -> void:
+	# Process movement
 	var frame_speed = get_frame_speed(delta)
 	self.position += frame_speed
+	
+	# Process action
+	if Input.is_action_just_pressed("action"):
+		action()
 
 
 func get_frame_speed(delta: float) -> Vector2:
@@ -23,3 +31,24 @@ func get_frame_speed(delta: float) -> Vector2:
 		frame_speed = frame_speed.normalized() * speed * delta
 	
 	return frame_speed
+
+
+func can_take() -> bool:
+	return $Inventory.get_children().size() == 0
+
+
+func take(object: Node) -> void:
+	$Inventory.add_child(object)
+
+
+func drop() -> void:
+	var drop_node = $Inventory.get_child(0)
+	remove_child(drop_node)
+	drop_request.emit(drop_node)
+
+
+func action() -> void:
+	if can_take():
+		take_request.emit()
+	else:
+		drop()
