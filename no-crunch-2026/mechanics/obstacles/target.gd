d64dc7b07c3c319f0518b03bd15318d7566fb433
MARCHAND MANON <manonmarchand22@gmail.com>
2026-05-15 17:07:58 +0200

    prevent animation from running twice

--- no-crunch-2026/mechanics/obstacles/target.gd
+++ no-crunch-2026/mechanics/obstacles/target.gd
@@ -1,8 +1,12 @@
 class_name Target
 extends Area2D
 
+var already_won: bool = false
+
 func _ready() -> void:
 	EventBus.victory.connect(_animate_victory)
 
 func _animate_victory():
-	$Sprite2D.play()
+	if not already_won:
+		already_won = true
+		$Sprite2D.play()
