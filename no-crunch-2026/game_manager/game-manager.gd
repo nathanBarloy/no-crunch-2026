2267f53f000aca3997aa6bab132d2418fdc686d2
nathanBarloy <nbarloy@gmail.com>
2026-05-15 17:13:01 +0200

    feat: Add game manager + placeholder title screen

---
+++ no-crunch-2026/game_manager/game-manager.gd
@@ -0,0 +1,28 @@
+extends Node
+
+var current_scene: Node = null
+var current_level_number: int = 0
+
+func unload_scene():
+	if current_scene:
+		current_scene.queue_free()
+		current_scene = null
+
+func level_exists(lvl_number: int) -> bool:
+	return ResourceLoader.exists("res://levels/%d/level.tscn" % lvl_number)
+
+func load_level(lvl_number: int):
+	unload_scene()
+	var scene = load("res://levels/%d/level.tscn" % lvl_number).instantiate()
+	get_tree().root.add_child(scene)
+	current_scene = scene
+	current_level_number = lvl_number
+
+func load_next_level():
+	if level_exists(current_level_number+1):
+		load_level(current_level_number+1)
+	else:
+		load_end_scene()
+
+func load_end_scene():
+	pass
