b11a7f2109a10bfe8a02476ce75f1f3932f63590
MARCHAND MANON <manonmarchand22@gmail.com>
2026-05-16 09:14:27 +0200

    art: mirror integration

--- no-crunch-2026/mechanics/obstacles/mirror.gd
+++ no-crunch-2026/mechanics/obstacles/mirror.gd
@@ -1,15 +1,3 @@
 class_name Mirror
 
 extends Area2D
-
-@export var is_tilted: bool = false
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	if is_tilted:
-		$Straight.visible = false
-		$Tilted.visible = true
-		$MirrorCollision.rotate(PI/4)
-	else:
-		$Tilted.visible = false
-		$Straight.visible = true
